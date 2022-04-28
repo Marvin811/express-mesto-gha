eb80b88f26855691dd08c049ee1c8d1a5579f22e
Egor Lukin <86829973+Marvin811@users.noreply.github.com>
2022-04-28 19:17:14 +0300

    fix: index.js

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,27 @@
 const router = require('express').Router();
-
+const { celebrate, Joi } = require('celebrate');
 const { createUser, login } = require('../controllers/users');
 const cardRoutes = require('./cards');
 const userRoutes = require('./users');
+const { auth } = require('../middlewares/auth');
 
-router.post('/signin', login);
-router.post('/signup', createUser);
+router.post('/signin', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(6),
+  }),
+}), login);
+router.post('/signup', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(6),
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+    avatar: Joi.string().pattern(/^(https?:\/\/)(www\.)?([\da-z-.]+)\.([a-z.]{2,6})[\da-zA-Z-._~:?#[\]@!$&'()*+,;=/]*\/?#?$/, 'URL'),
+  }),
+}), createUser);
 
-router.use('/users', userRoutes);
-router.use('/cards', cardRoutes);
+router.use('/users', auth, userRoutes);
+router.use('/cards', auth, cardRoutes);
 
 module.exports = router;

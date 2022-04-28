f53e578141045db70d4a4e277a6defdd3e7eee67
Egor Lukin <86829973+Marvin811@users.noreply.github.com>
2022-04-28 17:36:43 +0300

    feat: login

--- app.js
+++ app.js
@@ -2,10 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
-const { errors } = require('celebrate');
 const router = require('./routes/index');
-const NotFoundError = require('./errors/NotFoundError');
-const handleError = require('./errors/handleError');
 
 const { PORT = 3000 } = process.env;
 const app = express();
@@ -16,10 +13,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(router);
-app.use((req, res, next) => next(new NotFoundError('Ошибка: данный ресурс не найден.')));
-app.use(errors());
-
-app.use((err, res, req, next) => handleError({ res, err, next }));
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
--- controllers/users.js
+++ controllers/users.js
@@ -1,4 +1,5 @@
 /* eslint-disable consistent-return */
+const { NODE_ENV, JWT_SECRET } = process.env;
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const User = require('../models/user');
@@ -114,20 +115,24 @@ module.exports.createUser = (req, res, next) => {
     });
 };
 
-module.exports.login = (req, res) => {
+module.exports.login = (req, res, next) => {
   const { email, password } = req.body;
 
   return User.findUserByCredentials(email, password)
     .then((user) => {
-      res.send({
-        token: jwt.sign({ _id: user._id }, 'super-strong-secret', {
-          expiresIn: '7d',
-        }),
-      });
+      const token = jwt.sign(
+        { _id: user._id },
+        NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret',
+        { expiresIn: '7d' },
+      );
+
+      res.cookie('jwt', token, {
+        maxAge: 3600000 * 24 * 7,
+        httpOnly: true,
+      })
+        .send({ message: 'Логин успешный' });
     })
-    .catch((err) => {
-      res.status(401).send({ message: err.message });
-    });
+    .catch(next);
 };
 
 module.exports.updateUserInfo = (req, res, next) => {

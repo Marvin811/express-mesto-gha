262ff299913e6182984ec650414c91175bd29eb9
Egor Lukin <86829973+Marvin811@users.noreply.github.com>
2022-05-19 16:11:17 +0300

    refactor: env in auth

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,23 +1,27 @@
 require('dotenv').config();
+
+const { NODE_ENV, JWT_SECRET } = process.env;
 const jwt = require('jsonwebtoken');
 const UnauthorizedError = require('../errors/UnauthorizedError');
 
-const { NODE_ENV, JWT_SECRET } = process.env;
-
 module.exports = (req, res, next) => {
-  const token = req.cookies.jwt;
-  const secret = NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret';
-  if (!token) {
-    return next(new UnauthorizedError('Ошибка авторизации'));
+  const { authorization } = req.headers;
+
+  if (!authorization || !authorization.startsWith('Bearer ')) {
+    next(UnauthorizedError.unauthorized('Необходима авторизация'));
+    return;
   }
 
+  const token = authorization.replace('Bearer ', '');
   let payload;
   try {
-    payload = jwt.verify(token, secret);
-  } catch (err) {
-    return next(new UnauthorizedError('Необходима авторизация'));
+    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'some-dev-secret-key');
+  } catch (error) {
+    next(UnauthorizedError.unauthorized('Необходима авторизация'));
+    return;
   }
+
   req.user = payload;
 
-  return next();
+  next();
 };

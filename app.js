7f88c9dd0f02a7b6dde99b73e8f81916a13700b4
Egor Lukin <86829973+Marvin811@users.noreply.github.com>
2022-05-19 15:27:37 +0300

    fix: cors in app

--- app.js
+++ app.js
@@ -5,21 +5,27 @@ const bodyParser = require('body-parser');
 const { errors } = require('celebrate');
 const cookieParser = require('cookie-parser');
 const helmet = require('helmet');
-
+const cors = require('cors');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const { handleError } = require('./errors/handleError');
 const router = require('./routes/index');
 
-const allowedCors = [
-  'localhost:3000',
-  'http://localhost:3000',
-  'https://marvin.nomoredomains.xyz',
-  'http://marvin.nomoredomains.xyz',
-];
+const options = {
+  origin: [
+    'http://localhost:3000',
+    'https://marvin811.nomoredomains.xyz',
+    'https://marvin.nomoredomains.xyz',
+  ],
+  methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+  allowedHeaders: ['Content-Type', 'origin', 'Authorization'],
+  credentials: true,
+};
 
 const { PORT = 3000 } = process.env;
 const app = express();
-
+app.use('*', cors(options));
 app.use(helmet());
 
 mongoose.connect('mongodb://localhost:27017/mestodb');
@@ -30,26 +36,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.use(requestLogger); // подключаем логгер запросов
-
-// cors запросы
-app.use((req, res, next) => {
-  const { origin } = req.headers;
-  console.log(origin);
-  const { method } = req;
-  const requestHeaders = req.headers['access-control-request-headers'];
-  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
-  if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
-    res.header('Access-Control-Allow-Credentials', true);
-  }
-  if (method === 'OPTIONS') {
-    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-    res.header('Access-Control-Allow-Headers', requestHeaders);
-    return res.end();
-  }
-  next();
-  return null;
-});
 
 app.get('/crash-test', () => {
   setTimeout(() => {
